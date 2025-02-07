dc3099bceeee68bb26eebd9fb4fe5a5518f5b68b
W2F-C0d3R <110914061+W2F-Sa@users.noreply.github.com>
2025-02-07 12:50:31 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ set -e
 ufw disable
 
 # نصب Marzban
-sudo bash -c "$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh)" @ install
+
 
 # ویرایش فایل .env
 cat <<EOF | sudo tee /opt/marzban/.env
